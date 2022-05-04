32470947203be7f7ed7c95514c0e393438f785d9
Chirag Kohli <chirag.kohli@nagarro.com>
2022-05-04 13:13:18 +0530

    Commented code

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -15,17 +15,7 @@ const StyledDiv = styled.div`
     }
 `;
 const person = (props) => {
-    // const style = {
-    //     '@media(min-width: 500px)': {
-    //         width: '450px',
-    //     }
-    // };
     return (
-        // <div className='Person' style={style}>
-        //     <p onClick={props.click}>I am {props.name} and I am {props.age} Years old!</p>
-        //     <p>{props.children}</p>
-        //     <input type='text' value={props.name} onChange={props.changed} />
-        // </div>
         <StyledDiv>
             <p onClick={props.click}>I am {props.name} and I am {props.age} Years old!</p>
             <p>{props.children}</p>
